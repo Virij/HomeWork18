359284c4065b3f62508f72de48f9c28af8aa7423
virij80@gmail.com <virij80@gmail.com>
2021-03-20 21:39:54 +0300

    Добавьте файлы проекта.

---
+++ HomeWork18.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+
+class Stack
+{
+	std::vector<int> massive;
+	
+public:
+	void push(int newValue)
+	{
+		massive.push_back(newValue);
+	}
+	void pop()
+	{
+		massive.pop_back();
+	}
+	void show()
+	{
+		std::cout << "massive = { ";
+		for (int n : massive)
+		{
+			std::cout << n << ", ";
+		}
+		std::cout << "}; \n";
+	}
+};
+
+void main()
+{
+	// Создаем пустой стек
+	Stack stack;
+	std::cout << "Init: ";
+	// Смотрим содержимое стека
+	stack.show();
+
+	// Добавляем несколько элементов в стек
+	stack.push(3);
+	stack.push(5);
+	stack.push(8);
+
+	std::cout << "Filled: ";
+	// Смотрим содержимое стека
+	stack.show();
+	// Убираем из стека последний добавленный элемент
+	stack.pop();
+	std::cout << "After pop(): ";
+	// Смотрим содержимое стека
+	stack.show();
+	// Убираем из стека последний добавленный элемент
+	stack.pop();
+	std::cout << "after pop(): ";
+	// Смотрим содержимое стека
+	stack.show();
+
+}
